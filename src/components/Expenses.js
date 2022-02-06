c8f1d1cef116df3fbdd282f7e740c4a7bc79067d
LeecC1 <asjosten@catalystone.com>
2022-02-06 15:12:11 +0100

    First commit
    Create the repository

---
+++ src/components/Expenses.js
@@ -0,0 +1,20 @@
+import './Expenses.css';
+import Card from './Card';
+import ExpenseItem from './ExpenseItem';
+
+function Expenses(props) {
+    return (
+        <Card className='expenses'>
+        {props.items.map((item) => 
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        )}
+        </Card>        
+    )
+}
+
+export default Expenses;
